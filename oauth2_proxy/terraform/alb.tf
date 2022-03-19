4db6e4fcd8f1de77f44233e44ecb9c3d04c3fc3b
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-03-19 18:56:21 -0400

    feat: redirect http to https

--- oauth2_proxy/terraform/alb.tf
+++ oauth2_proxy/terraform/alb.tf
@@ -38,6 +38,22 @@ resource "aws_lb_target_group" "ecs" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.buzzfeed_sso.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.buzzfeed_sso.arn
 
